b65281df337726542b38ac5fe4519b86e28266df
adrianGette <adrian.gette@icloud.com>
2021-04-05 17:46:04 -0300

    Inicio ejercicio 1

---
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// Realizar un programa capaz de calcular el área
+/// y perímetro de un cuadrado, rectángulo y triángulo.
+/// Todos los datos necesarios para su funcionamiento
+/// se ingresan por teclado.
+/// 
+/// </summary>
+
+namespace Clase1.Ejercicio1.Figuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            Console.ReadLine();
+        }
+    }
+}
