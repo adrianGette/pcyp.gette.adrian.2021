c0f4cc4dad82c6f5a2f6d3557d2d893541c8bebf
adrianGette <adrian.gette@icloud.com>
2021-04-06 14:14:50 -0300

    Inicio Ejercicio 2
    Iniciando el ejercicio.

---
+++ PCyP.Ejercicios/Clase1.Ejercicio2.Vial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio2.Vial
+{
+
+    /// <summary>
+    /// Recorrer el archivo vial.csv, tomar cada linea, 
+    /// separar los campos y mostrarlos por pantalla. 
+    /// Instanciar un objeto por cada linea  que pueda 
+    /// registrar los campos necesarios y que exponga un 
+    /// método imprimir() donde muestre esos valores.
+    /// 
+    //  Los campos del archivo CSV son: 
+    //  número-de-hoja,tipo-objeto,identificador-tramo,tipo-camino,longitud
+    /// </summary>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 2");
+            Console.ReadLine();
+        }
+    }
+}
