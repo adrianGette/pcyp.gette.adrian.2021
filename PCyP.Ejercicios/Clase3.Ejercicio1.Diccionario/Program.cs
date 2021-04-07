f5dac65e0ea800caae6a0a889369a07dd7316122
adrianGette <adrian.gette@icloud.com>
2021-04-07 15:02:20 -0300

    Inicio Ejercicio 1 Clase 3
    Commit y Push del proyecto a github.com

---
+++ PCyP.Ejercicios/Clase3.Ejercicio1.Diccionario/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio1.Diccionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clase3.Ejercicio1.Diccionario");
+            Console.ReadLine();
+        }
+    }
+}
