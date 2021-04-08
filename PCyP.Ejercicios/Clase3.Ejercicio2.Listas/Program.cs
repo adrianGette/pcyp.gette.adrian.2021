d67474e703e304e712f1664cd18b41eead56c7da
adrianGette <adrian.gette@icloud.com>
2021-04-08 15:42:50 -0300

    Puntos A y B
    Puntos A y B del ejercicio 2 completos.

--- PCyP.Ejercicios/Clase3.Ejercicio2.Listas/Program.cs
+++ PCyP.Ejercicios/Clase3.Ejercicio2.Listas/Program.cs
@@ -6,11 +6,45 @@ using System.Threading.Tasks;
 
 namespace Clase3.Ejercicio2.Listas
 {
+
+    /// <summary>
+    /// 2. Manejo básico de listas
+    /// 
+    /// Dado dos vectores:
+    /// 
+    /// private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+    /// private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+    /// 
+    /// Realice las siguientes operaciones:
+    /// 
+    /// a.Armar dos listas, una con los valores del vector “colors” y otra con los valores del
+    /// vector “removeColors”.
+    /// 
+    /// b.Mostrar la lista de colores por pantalla.
+    /// 
+    /// c.De la lista de colores eliminar los colores listados en la otra lista(removeColors).
+    /// 
+    /// d.Volver a mostrar la lista de colores por pantalla.
+    /// </summary>
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Clase3.Ejercicio2.Listas");
+            // Punto A
+            List<string> colors = new List<string>();
+            List<string> removeColors = new List<string>();
+
+            Vectores.colores(colors);
+            Vectores.removerColores(removeColors);
+
+            // Punto B
+            foreach (string col in colors)
+            {
+                Console.WriteLine(col);
+            }
+            
+
             Console.ReadLine();
         }
     }
---
+++ PCyP.Ejercicios/Clase3.Ejercicio2.Listas/Vectores.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio2.Listas
+{
+    class Vectores
+    {
+        private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+        
+        public static List<string> colores(List<string> lista1)
+        {
+            foreach (string color in colors)
+            {
+                lista1.Add(color);
+            }
+
+            return lista1;
+        }
+
+        public static List<string> removerColores(List<string> lista2)
+        {
+            foreach (string color in removeColors)
+            {
+                lista2.Add(color);
+            }
+
+            return lista2;
+        }
+
+    }
+}
