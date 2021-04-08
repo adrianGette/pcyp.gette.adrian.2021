920be41d007ace89fb3de70fe42c85f557388d06
adrianGette <adrian.gette@icloud.com>
2021-04-08 15:00:14 -0300

    Inicio Ejercicio 2 Clase 3
    Primer commit y subida del ejercicio.

---
+++ PCyP.Ejercicios/Clase3.Ejercicio2.Listas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio2.Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clase3.Ejercicio2.Listas");
+            Console.ReadLine();
+        }
+    }
+}
